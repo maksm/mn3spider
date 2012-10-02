e37091ac17e22503fec94e0b349204592a97220a
maksm <maks.mrzek@gmail.com>
2012-10-02 11:31:56 +0200

    almost there

--- mn3spider/spiders/mn3_spider.py
+++ mn3spider/spiders/mn3_spider.py
@@ -8,21 +8,26 @@ from mn3spider.db import SqliteDB
 # more than 1000 views or replies could not be counted
 # when logged in, urls don't contain session variables anymore, so no processing is needed
 db = SqliteDB()
+db.create()
 class Mn3Topic(BaseSpider):
     name = "mn3topic"
     allowed_domains = ["joker.si"]
-    start_urls = ["http://www.joker.si/mn3njalnik/index.php?showforum=14"]
+    start_urls = []
+    forums = db.getForums()
+    for forum in forums:
+        url = "http://www.joker.si/mn3njalnik/index.php?showforum="+str(forum[0])
+        start_urls.append(url)
     
-    def parse(self, response,):
+    def parse(self, response):
         hxs = HtmlXPathSelector(response)
         forumid = 14
         #check if last page
         next = hxs.select('//ul[@class="ipsList_inline forward left"]//li[@class="next"]//a/@href').extract()
         if len(next) > 0:
-            a = next[0].split('?')
-            b = a[1].split('&')
-            url = a[0]+"?"+b[1]+"&"+b[2]+"&"+b[3]+"&"+b[4]+"&"+b[5]+"&"+b[6]
-            yield Request(url,self.parse)
+#            a = next[0].split('?')
+#            b = a[1].split('&')
+#            url = a[0]+"?"+b[1]+"&"+b[2]+"&"+b[3]+"&"+b[4]+"&"+b[5]+"&"+b[6]
+            yield Request(next[0],self.parse)
 
         topic_list = hxs.select('//div[@class="ipsBox"]//div[@class="ipsBox_container"]//table//tr')[1:]
         for topic in topic_list:
@@ -31,21 +36,21 @@ class Mn3Topic(BaseSpider):
             item['author'] = topic.select('td[@class="col_f_content "]//a[@class="_hovertrigger url fn "]//text()').extract()[0]
             date = topic.select('td[@class="col_f_content "]//a[@class="topic_title"]//@title').extract()[0]
             a = date.split()
-            item['date']  = a[3]+" "+a[4]+" "+a[5]+" "+a[6]+" "+a[7]
+            item['date']  = date#a[3]+" "+a[4]+" "+a[5]+" "+a[6]+" "+a[7]
             url = topic.select('td[@class="col_f_content "]//a[@class="topic_title"]//@href').extract()[0]
-            a = url.split('?')
-            b = a[1].split('&')
-            url = a[0]+"?"
-            if len(b) > 1:
-                url = url+b[1]
-            else:
-                url = url+b[0]
-            item['url'] = url
+#            a = url.split('?')
+#            b = a[1].split('&')
+#            url = a[0]+"?"
+#            if len(b) > 1:
+#                url = url+b[1]
+#            else:
+#                url = url+b[0]
+#            item['url'] = url
             item['id'] = url.split('=')[-1]
             replies = topic.select('td[@class="col_f_views desc blend_links"]//a//text()').extract()[0]
             item['replies'] = replies.split()[0]
             item['views'] = topic.select('td[@class="col_f_views desc blend_links"]//li[@class="views desc"]//text()').extract()[0]
-            db.insertTopic(forumid,  item['id'],  item['title'],  item['author'],  item['date'],  item['url'])
+            db.insertTopic(forumid,  item['id'],  item['title'],  item['author'],  item['date'])
             #yield item
 
 
@@ -78,6 +83,6 @@ class Mn3Post(BaseSpider):
             item['date'] = post.select('div[@class="post_body"]/p/abbr/text()').extract()[0]
             content = post.select('div[@class="post_body"]/div').extract()[0]
             item['content']  = content[84:-10]
-            db.insertPost(topicid,  tem['id'] ,  item['num'],  item['author'],  item['date'],  item['content'])
-            yield item
+            db.insertPost(topicid,  item['id'] ,  item['num'],  item['author'],  item['date'],  item['content'])
+            #yield item
 
